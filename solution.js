e5416da928d3568991e58e7b82f121bbafae6e5a
Chanda Mulenga <stconeten@gmail.com>
2019-06-14 02:06:43 +0200

    Completes task 1 and task 2

--- solution.js
+++ solution.js
@@ -1,1 +1,10 @@
 // solutions to the exercises mentioned in the readme should be in this file.
+const largerInt = (a, b) => (a > b ? console.log(a) : console.log(b));
+
+for (let i = 0; i <= 15; i++) {
+	if (i % 2 === 0) {
+		console.log(`${i} is even`);
+	} else {
+		console.log(`${i} is odd`);
+	}
+}
