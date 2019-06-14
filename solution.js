5b3079bc0e5fe8ee2c34a59882e70b0904da7d06
Chanda Mulenga <stconeten@gmail.com>
2019-06-14 12:17:26 +0200

    Completes task 3

--- solution.js
+++ solution.js
@@ -1,6 +1,8 @@
 // solutions to the exercises mentioned in the readme should be in this file.
 const largerInt = (a, b) => (a > b ? console.log(a) : console.log(b));
 
+largerInt(5, 10); // Output should be 10
+
 for (let i = 0; i <= 15; i++) {
 	if (i % 2 === 0) {
 		console.log(`${i} is even`);
@@ -8,3 +10,16 @@ for (let i = 0; i <= 15; i++) {
 		console.log(`${i} is odd`);
 	}
 }
+
+const sumOfMultis = () => {
+	let sum = 0;
+	let i = 0;
+
+	while (i < 1000) {
+		i++;
+		i % 3 === 0 || i % 5 === 0 ? (sum += i) : '';
+	}
+	console.log(`The sum is ${sum}`);
+};
+
+sumOfMultis();
